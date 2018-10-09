2735cec6f17f72e68461e5128f4a2504f67e849d
Dylan <37284327+DigitalAtrophy@users.noreply.github.com>
2018-10-08 22:09:55 -0700

    Update for Chapter 5 assignment

--- JS/index.js
+++ JS/index.js
@@ -1,175 +1,42 @@
 /* 
 	Author: Dylan Kirby
-	Date: 2018-09-30
+	Date: 2018-10-08
 	Filename: index.js
 */
 
-//global variables for validity check
-var validHeight = false;
-var validWeight = false;
-var height = 0;
-
-//function to test for number
-function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-//series of try catch blocks to catch exceptions
-function verifyHeight()
+//function to obtain identifying info about user
+function getPersonalInfo()
 {
-	try
-	{
-		height = document.getElementById("height").value;
-		
-		if (height === "")
-		{
-			throw "Please enter your height.";
-		}
-		else
-		{
-			try
-			{
-				if (isNumber(height))
-				{
-					try
-					{
-						if (height <= 0)
-						{
-							throw "Please enter a height greater than 0.";
-						}
-						else
-						{
-							validHeight = true;
-						}
-					}
-					catch(heightError)
-					{
-						window.alert(heightError);
-					}
-				}
-				else
-				{
-					throw "Please enter a number for your height.";
-				}
-			}
-			catch(heightError)
-			{
-				window.alert(heightError);
-			}
-		}
-	}
-	catch(heightError)
-	{
-		window.alert(heightError);
-	}
-}
-
-function verifyWeight()
-{
-	try
-	{
-		weight = document.getElementById("weight").value;
-		
-		if (weight === "")
-		{
-			throw "Please enter your weight.";
-		}
-		else
-		{
-			try
-			{
-				if (isNumber(weight))
-				{
-					try
-					{
-						if (weight <= 0)
-						{
-							throw "Please enter a weight greater than 0.";
-						}
-						else
-						{
-							validWeight = true;
-						}
-					}
-					catch(weightError)
-					{
-						window.alert(weightError);
-					}
-				}
-				else
-				{
-					throw "Please enter a number for your weight.";
-				}
-			}
-			catch(weightError)
-			{
-				window.alert(weightError);
-			}
-		}
-	}
-	catch(weightError)
-	{
-		window.alert(weightError);
-	}
-}
-
-//function to test for valid inputs
-function testValidity()
-{
-	document.getElementById("bmi").innerHTML = "";
-	verifyHeight();
+	//variables to store values
+	var browser;
+	var browserVersion;
+	var OS;
+	var resolution;
+	var screenHeight;
+	var screenWidth;
 	
-	if (validHeight)
-	{
-		verifyWeight();
-		
-		if (validWeight)
-		{
-			calcBMI();
-		}
-	}
+	//retrieves values
+	browser = navigator.appName;
+	browserVersion = navigator.appVersion;
+	OS = navigator.platform;
+	resolution = screen.pixelDepth;
+	screenHeight = screen.height;
+	screenWidth = screen.width;
+	
+	//writes values
+	document.getElementById("browser").textContent += browser;
+	document.getElementById("browserVersion").textContent += browserVersion;
+	document.getElementById("OS").textContent += OS;
+	document.getElementById("resolution").textContent += resolution;
+	document.getElementById("screenHeight").textContent += screenHeight;
+	document.getElementById("screenWidth").textContent += screenWidth;
 }
 
-function calcBMI()
+if (window.addEventListener)
 {
-	//var height = 0;
-	var weight = 0;
-	var bmi = 0;
-			
-	if (document.getElementById("inches").checked) //converts to meters if inches
-	{
-		height = height * 0.0254;
-	}
-	else if (document.getElementById("centimeters").checked) //converts cm to m
-	{
-		height = height * 0.01;
-	}
-			
-	if (document.getElementById("pounds").checked) //converts lbs to kg
-	{
-		weight = weight * 0.453592;
-	}
-	else if (document.getElementById("kilograms").checked)
-	{
-		weight = document.getElementById("weight").value * 1;
-	}
-			
-	bmi = weight / (height * height);
-	
-	//clears input fields and resets validity checks
-	document.getElementById("bmi").innerHTML = "Your BMI is " + bmi.toFixed(1) + ".";
-	document.getElementById("height").value = "";
-	document.getElementById("weight").value = "";
-	validHeight = false;
-	validWeight = false;
+	window.addEventListener("load", getPersonalInfo, false);
 }
-		
-var submitButton = document.getElementById("submit");
-if (submitButton.addEventListener)
+else if (window.attachEvent)
 {
-	submitButton.addEventListener("click", testValidity, false);
-}
-else if (submitButton.attachEvent)
-{
-	submitButton.attachEvent("onclick", testValidity);
+	window.attachEvent("onload", getPersonalInfo);
 }
